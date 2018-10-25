9e4eb0964be5be278da4f27fa250c3c404f92eac
Renaat De Muynck <renaat.demuynck@gmail.com>
2018-10-25 15:37:24 +0200

    Add polyfills to make code compatible with older browsers (IE 11)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import '@babel/polyfill';
+
 import canvas from '@ahsdile/canvas-lms-app';
 
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,6 @@ module.exports = {
     module: {
         rules: [{
             test: /\.js$/,
-            exclude: /node_modules/,
             use: [{
                 loader: 'babel-loader',
                 options: {
