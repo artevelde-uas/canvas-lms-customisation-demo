d0ee034e0b15ced9a7deed393b2b67c00851f7e0
Renaat De Muynck <renaat.demuynck@gmail.com>
2018-10-25 16:08:21 +0200

    Add some cool plugins

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import '@babel/polyfill';
 
 import canvas from '@ahsdile/canvas-lms-app';
+import enableCourseRecycleBinPlugin from '@ahsdile/canvas-lms-enable-course-recycle-bin-plugin';
+import reorderDashboardCoursesPlugin from '@ahsdile/canvas-lms-reorder-dashboard-courses-plugin';
+import '@ahsdile/canvas-lms-sticky-left-menu-plugin/index.css';
 
+
+canvas.addPlugin(enableCourseRecycleBinPlugin);
+canvas.addPlugin(reorderDashboardCoursesPlugin);
 
 canvas.run();
