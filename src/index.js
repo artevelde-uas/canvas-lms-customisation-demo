f97d770f18d4ebca2656e21aa0d6d47cc94feda0
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-05-11 13:53:07 +0200

    Add some new plug-ins

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import canvas from '@artevelde-uas/canvas-lms-app';
 
+import allCoursesTermsTabsPlugin from '@artevelde-uas/canvas-lms-all-courses-terms-tabs-plugin';
+import conferenceEditorGroupFilterPlugin from '@artevelde-uas/canvas-lms-conference-editor-group-filter-plugin';
+import enableCourseRecycleBinPlugin from '@artevelde-uas/canvas-lms-enable-course-recycle-bin-plugin';
+import exportCourseGroupsPlugin from '@artevelde-uas/canvas-lms-export-course-groups-plugin';
+import fileExplorerEnhancementsPlugin from '@artevelde-uas/canvas-lms-file-explorer-enhancements-plugin';
+import fixFilesNavigationPlugin from '@artevelde-uas/canvas-lms-fix-files-navigation-plugin';
+
+
+canvas.addPlugin(allCoursesTermsTabsPlugin);
+canvas.addPlugin(conferenceEditorGroupFilterPlugin);
+canvas.addPlugin(enableCourseRecycleBinPlugin);
+canvas.addPlugin(exportCourseGroupsPlugin);
+canvas.addPlugin(fileExplorerEnhancementsPlugin);
+canvas.addPlugin(fixFilesNavigationPlugin);
+
 canvas.run();
